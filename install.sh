539e54ebaf2238f4e8f7c2ec0a58eef29ff788ef
Brieuc Jamain <brieucjamain@gmail.com>
2022-03-21 23:37:51 +0100

    setup git

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 cp -a ./files/. ~/
+
+#setup git
+git config --global core.excludesfile ~/.gitignore_global
+git config --global user.name "Brieuc Jamain"
+git config --global user.email brieucjamain@gmail.com
+
 source ~/.bash_profile
 echo 'DONE'
